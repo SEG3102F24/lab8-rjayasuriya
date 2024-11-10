3939308e495a89a783de3ff9655612ce279b38b3
Eshal Kamran <ekamr048@uottawa.ca>
2024-11-09 19:24:14 -0500

    Implemented GraphQL server for employee data

--- src/main/kotlin/seg3x02/employeeGql/entity/Employee.kt
+++ src/main/kotlin/seg3x02/employeeGql/entity/Employee.kt
@@ -3,15 +3,12 @@ package seg3x02.employeeGql.entity
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Document(collection = "employee")
+@Document(collection = "employees")
 data class Employee(
-        val name: String,
-        val dateOfBirth: String,
-        val city: String,
-        val salary: Float,
-        val gender: String?,
-        val email: String?
-) {
     @Id
-    var id: String = ""
-}
+    var employeeId: String? = null,
+    var firstName: String,
+    var lastName: String,
+    var position: String,
+    var department: String
+)
--- src/main/kotlin/seg3x02/employeeGql/resolvers/types/CreateEmployeeInput.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/types/CreateEmployeeInput.kt
@@ -1,11 +1,8 @@
-package seg3x02.employeeGql.resolvers.types
+package seg3x02.employeeGql.types
 
-class CreateEmployeeInput (
-    val name: String? = null,
-    val dateOfBirth: String?  = null,
-    val city: String? = null,
-    val salary: Float? = null,
-    val gender: String? = null,
-    val email: String?  = null
+data class CreateEmployeeInput(
+    val firstName: String,
+    val lastName: String,
+    val position: String,
+    val department: String
 )
-
